e7bffb68722c0dd69cb05d22de060045f92d7a3b
Maximiliano Brandi <maxi.brandi.2011@gmail.com>
2024-09-21 23:58:29 -0300

    feat: agregando avatar

--- proyecto/home/forms.py
+++ proyecto/home/forms.py
@@ -1,13 +1,19 @@
-# from django.contrib.auth.models import User
-# from django.contrib.auth.forms import UserCreationForm
-# from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from .models import UserProfile
 
-# class CustomUserCreationForm(UserCreationForm):
-#     class Meta:
-#         model = User
-#         fields = ('username', 'password1','password2',)
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('username','email','password1','password2')   
 
-# class UserProfileForm(forms.ModelForm):
-#     class Meta:
-#         model = User
-#         fields = '__all__'
+class UserUpdateForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('username', 'email')  # Campos del usuario
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ['avatar']  # Solo el campo del avatar
--- proyecto/home/urls.py
+++ proyecto/home/urls.py
@@ -1,13 +1,19 @@
-# from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 from . import views
 
 app_name = 'home'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    # path('login', LoginView.as_view(template_name='home/login.html'), name='login'),
-    # path('logout', LogoutView.as_view(template_name='home/logout.html'), name='logout'),
-#     path('register', views.Register.as_view(), name='register'),
-#     path('profile', views.Profile.as_view(), name='profile'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', LogoutView.as_view(template_name='home/logout.html'), name='logout'),
+    path('register/', views.Register.as_view(), name='register'),
+    path('profile/', views.Profile.as_view(), name='profile'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
