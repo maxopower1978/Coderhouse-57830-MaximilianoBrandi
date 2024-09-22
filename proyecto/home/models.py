ad612fed6ddf21b26e3bfa1e238e0a0eb73c1358
Maximiliano Brandi <maxi.brandi.2011@gmail.com>
2024-09-22 11:13:56 -0300

    feat: modificando avatares

--- proyecto/home/models.py
+++ proyecto/home/models.py
@@ -1,6 +1,9 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    avatar = models.ImageField(upload_to='avatares/', null=True, blank=True)  # Asegúrate de que el upload_to sea 'avatares/'
+    image = models.ImageField(upload_to='avatares/', null=True, blank=True) 
+    
+    def __str__(self): # Asegúrate de que el upload_to sea 'avatares/'
+        return f"{self.user} - {self.image}"
